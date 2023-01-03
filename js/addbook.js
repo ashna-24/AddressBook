f99e83aa823a58e0901d20d162757780b46a01b8
ashna-24 <ashnafrajan@gmail.com>
2023-01-03 18:30:12 +0530

    commit

---
+++ js/addbook.js
@@ -0,0 +1,43 @@
+function validateform(){
+    var uname= document.getElementById('uname');
+    var pswd= document.getElementById('pswd');
+
+    var unamevalidate = validateuname(uname);
+    var pswdvalidate = validatepswd(pswd);
+    if(unamevalidate && pswdvalidate)
+        return true;
+    else
+        return false;
+}
+
+function errorValidate(id){
+    document.getElementById(id).style.visibility="visible";
+}
+  
+function successValidate(id){
+    document.getElementById(id).innerHTML="";
+}
+
+function mailValidate(uname){
+    if(uname.value==""){
+        errorValidate('uname_error');
+        uname.style.border="1px solid red";  
+        return false;
+    }else{
+        successValidate('uname_error');
+        uname.style.border="1px solid green";
+        return true;
+    }
+}
+
+function mailValidate(pswd){
+    if(pswd.value==""){
+        errorValidate('pswd_error');
+        pswd.style.border="1px solid red";  
+        return false;
+    }else{
+        successValidate('pswd_error');
+        pswd.style.border="1px solid green";
+        return true;
+    }
+}
---
+++ sql/query.sql
@@ -0,0 +1,4 @@
+use query;
+
+create table addressBook(Username varchar(20)
+                        Password varchar(20));
